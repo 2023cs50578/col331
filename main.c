573afca0162d16266e8d6a01e0a2e0dab1b5320f
Priyanka Sar <priyankasar@Priyankas-MacBook-Air-3.local>
2026-03-09 23:54:43 +0530

    removed unused extern char[]

--- main.c
+++ main.c
@@ -2,14 +2,12 @@
 #include "defs.h"
 #include "x86.h"
 
-extern char end[]; // first address after kernel loaded from ELF file
-
 int
 halt(void)
 {
-  cprintf("Bye COL%d!\n\0", 331);
+  cprintf("Bye COL%d!\n", 331);
   outw(0x604, 0x2000);
-  // For older versions of QEMU, 
+  // For older versions of QEMU, use port 0xB004.
   outw(0xB004, 0x2000);
   return 0;
 }
